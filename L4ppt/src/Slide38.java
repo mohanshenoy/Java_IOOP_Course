8c44cdf73c7fef25033743fc62871859d393fb4f
Mohan Shenoy <mohanshenoy@gmail.com>
2026-01-24 17:50:21 +0530

    updated L4ppt project

--- L4ppt/src/Slide38.java
+++ L4ppt/src/Slide38.java
@@ -4,7 +4,7 @@ public class Slide38 {
 	public static void main(String[] args) {
 		boolean t = true;
 		System.out.println("before the return");
-		if (t)
+		//if (t) 
 		    return; // Return to caller
 		System.out.println("This wont execute");
 
