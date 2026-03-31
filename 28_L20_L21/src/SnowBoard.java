92ad93ef4b5bd2fd6cdb8f4401ca9f1bf75efe50
Mohan Shenoy <mohanshenoy@gmail.com>
2026-03-31 11:41:53 +0530

    updated foles in L20_L21

--- 28_L20_L21/src/SnowBoard.java
+++ 28_L20_L21/src/SnowBoard.java
@@ -40,7 +40,6 @@ public class SnowBoard implements Cloneable{
 	
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
 		return super.clone();
 	}
 	
